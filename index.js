441ab7d6b25a55920345e733c928f1cc8bd6256d
Your Name <abdullahi.abass@student.moringschool.com>
2023-07-08 19:53:34 +0300

    Completed

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const alphanumericWord = word.replace(/[^a-zA-Z0-9]/g, '');
+  const lowercaseWord = alphanumericWord.toLowerCase();
+  const reversedWord = lowercaseWord.split('').reverse().join('');
+  return lowercaseWord === reversedWord;
 }
 
 /* 
-  Add your pseudocode here
+  Pseudocode:
+  1. Remove any non-alphanumeric characters from the word.
+  2. Convert the word to lowercase for case-insensitive comparison.
+  3. Reverse the word.
+  4. Check if the reversed word is equal to the original word.
+  5. Return true if they are equal, false otherwise.
 */
 
-/*
-  Add written explanation of your solution here
-*/
+// Custom tests
+console.log("Expecting: true");
+console.log("=>", isPalindrome("racecar"));
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+console.log("");
 
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+console.log("Expecting: false");
+console.log("=>", isPalindrome("robot"));
 
 module.exports = isPalindrome;
